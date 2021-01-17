2b22ad4d234bbfc8c47fd14e90863d4a1ba7491c
Hemanth21k <satyasaihemanth.p18@iiits.in>
2021-01-17 17:44:31 +0530

    added pose comparision

--- pose-est/src/App.js
+++ pose-est/src/App.js
@@ -9,6 +9,64 @@ import captureVideoFrame from "capture-video-frame/capture-video-frame.js";
 import ReactDOM from 'react-dom';
 // import { Player } from 'video-react';
 
+function get_score(pose1,pose2,minConfidence){
+  console.log("getting score")
+  let p1Array = [];
+  let p2Array = [];
+
+  for (let i = 5; i < pose1.length; i++) {
+    let p1keypoint = pose1[i];
+    let p2keypoint = pose2[i];
+    console.log(p1keypoint.score,p2keypoint.score)
+    if (p1keypoint.score < minConfidence || p2keypoint.score < minConfidence) {
+      // console.log("Exiting score")
+      continue;
+    }
+    // console.log(p1keypoint.position)
+    let x1 = p1keypoint.position.x;
+    let y1 = p1keypoint.position.y;
+    console.log(x1,y1)
+    p1Array.push(x1,y1)  
+
+    let x2 = p2keypoint.position.x;
+    let y2 = p2keypoint.position.y;
+    p2Array.push(x2,y2)
+  } 
+
+
+// console.log(p1Array,p2Array)
+//cosine sim
+let num=0;
+let d1 = 0;
+let d2 = 0;
+
+for (let j=0;j<p1Array.length;j++){
+  num += p1Array[j] * p2Array[j];
+  d1 += p1Array[j] ** 2;
+  d2 += p2Array[j] ** 2;
+}
+
+d1 = Math.sqrt(d1)
+d2 = Math.sqrt(d2)
+
+
+if (d1!=0 || d2!=0){
+
+  let cosineSim = num/(d1*d2);
+  let EuclidDist = 2 * (1-cosineSim);
+  EuclidDist = Math.sqrt(EuclidDist);
+  console.log("CosScore:", cosineSim,"EuclidSCORE:" ,EuclidDist)
+  return [cosineSim,EuclidDist];
+
+}
+else{
+  return [0,0];
+}
+
+}
+  
+
+
 function convertURIToImageData(URI) {
   return new Promise(function(resolve, reject) {
     if (URI == null) return reject();
@@ -24,25 +82,29 @@ function convertURIToImageData(URI) {
     image.src = URI;
   });
 }
+
+
+
 class App extends React.Component{
-	constructor (props){
-		super(props);
+  constructor (props){
+    super(props);
+    this.canvasRef = React.createRef(null);
+    this.webcamRef = React.createRef(null);
+    this.state = {
+      cosScore :0,
+      EuclidScore:0,
+      frameImg: 0
+    };
 
-		this.state = {
+  }
+  
 
-			frameImg: ''
-		};
-
-	}
-	
-
-	render(){const webcamRef = React.createRef(null);
-	const canvasRef = React.createRef(null);
+  render(){
 
   const runPosenet = async () => {
     const posenet_model = await posenet.load({
-    	architecture: 'MobileNetV1',
-  		outputStride: 16,
+      architecture: 'MobileNetV1',
+      outputStride: 16,
       inputResolution: { width: 320, height: 224 },
       multiplier: 0.75
     });
@@ -53,40 +115,52 @@ class App extends React.Component{
     }, 100);
 };
 
-	const detectWebcamFeed = async (posenet_model) => {
+  const detectWebcamFeed = async (posenet_model) => {
     if ( // make sure webcam are avaliable
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
+      typeof this.webcamRef.current !== "undefined" &&
+      this.webcamRef.current !== null &&
+      this.webcamRef.current.video.readyState === 4
     ) {
       // Grab Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+      const video = this.webcamRef.current.video;
+      const videoWidth = this.webcamRef.current.video.videoWidth;
+      const videoHeight = this.webcamRef.current.video.videoHeight;
       // Adjust video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+      this.webcamRef.current.video.width = videoWidth;
+      this.webcamRef.current.video.height = videoHeight;
 // Make Estimation
+
       const pose = await posenet_model.estimateSinglePose(video);
-      console.log(pose)
-      drawResult(pose, video, videoWidth, videoHeight, canvasRef);
+      // console.log("Cam:",pose)
+      drawResult(pose, video, videoWidth, videoHeight, this.canvasRef);
 
 
       const frame = captureVideoFrame(this.player.getInternalPlayer())
-      console.log("captured frame: ",frame)
-      const frameWidth = this.player.videoWidth
-      const frameHeight = this.player.videoHeight
-      var frameImage = new Image;
-      frameImage.src = frame.dataUri
+//       console.log("captured frame: ",frame)
+//       // const frameWidth = this.player.videoWidth
+// //       // const frameHeight = this.player.videoHeight
 convertURIToImageData(frame.dataUri).then(imageData => this.setState({
   // Here you can use imageData
 
   frameImg:imageData
 }));
-      console.log(this.state.frameImg)
+//       console.log(this.state.frameImg)
+    if (this.state.frameImg != 0){
       const framePose = await posenet_model.estimateSinglePose(this.state.frameImg)
-      console.log("FramePose: ",framePose)
+      // console.log("Frame:",framePose)
+      // console.log("FramePose: ",framePose)
       // drawResult(framePose,this.state.frameImg,videoWidth,videoHeight,canvasRef)
+
+      const [cosSim,euSim] = get_score(pose['keypoints'],framePose['keypoints'],0.3)
+
+      // console.log("CosScore:", cosSim,"Euclid:" ,euSim)
+
+      this.setState({
+        cosScore:cosSim,
+        EuclidScore:euSim
+      });
+    }
+      // console.log("Frame : CosScore:", this.state.cosScore,"Euclid:" ,this.state.EuclidScore)
     }
   };
   runPosenet();
@@ -104,21 +178,28 @@ convertURIToImageData(frame.dataUri).then(imageData => this.setState({
   return (
     <div className="App">
       <header className="App-header">
-      	<ReactPlayer
-      		ref={player => { this.player = player }}
-      		url = 'test.mp4'
-      		controls = {true}
-      		style = {{position:"absolute",
-      					left:20
+        <div className = "Scores" style = {{position:"absolute",top:20}}>
+          <div> CosineScore: {this.state.cosScore}</div>
+          <div> EuclidScore: {this.state.EuclidScore} </div>
+        </div>
+        
 
-      				}}
-      		config={{ file: { attributes: {
-					    crossorigin: 'anonymous'
-					  }}}}
-      	/>
 
-      	<Webcam
-          ref={webcamRef}
+        <ReactPlayer
+          ref={player => { this.player = player }}
+          url = 'test.mp4'
+          controls = {true}
+          style = {{position:"absolute",
+                left:20
+
+              }}
+          config={{ file: { attributes: {
+              crossorigin: 'anonymous'
+            }}}}
+        />
+
+        <Webcam
+          ref={this.webcamRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -133,7 +214,7 @@ convertURIToImageData(frame.dataUri).then(imageData => this.setState({
 
 
         <canvas
-          ref={canvasRef}
+          ref={this.canvasRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -150,7 +231,7 @@ convertURIToImageData(frame.dataUri).then(imageData => this.setState({
     </div>
   );
 
-	}
+  }
 
 
 }
